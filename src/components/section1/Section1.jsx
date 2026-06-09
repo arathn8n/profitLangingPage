df85673d6aa5bac479378e4b9d1aa64412ac02a4
arath abello <arathabello60@gmail.com>
2026-06-09 11:06:58 -0400

    terminando section 1

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -3,7 +3,14 @@ import "./section1.scss"
 const Section1 = () => {
     return (
         <div className='bgImage'>
-
+            <section className='section1'>
+                <header>
+                    <h1 className='big-title'>Best <br />
+                        Fitness</h1>
+                </header>
+                <p className='pSection1'>Our club encourages wellness by providing top-notch equipment, first-class instructors, innovative classes, and qualified staff.</p>
+                <a href="#" className='linkJoin'>JOIN NOW</a>
+            </section>
         </div>
     )
 }
