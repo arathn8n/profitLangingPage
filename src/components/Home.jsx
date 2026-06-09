34e723458359ca044a50754ef9be321e8a1f1f01
arath abello <arathabello60@gmail.com>
2026-06-09 10:48:35 -0400

    comenzando section1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './header/Header'
+import Section1 from './section1/Section1'
 
 const Home = () => {
     return (
         <>
             <Header />
+            <Section1 />
         </>
     )
 }
---
+++ src/components/section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./section1.scss"
+const Section1 = () => {
+    return (
+        <div className='bgImage'>
+
+        </div>
+    )
+}
+
+export default Section1
