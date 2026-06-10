5686003302ddd3f5158ad949dede5fecea3315a1
arath abello <arathabello60@gmail.com>
2026-06-09 21:17:54 -0400

    comenzando section 4

---
+++ src/components/section4/Section4.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Section4 = () => {
+    return (
+        <section>
+            <header>
+                <h2>welcome to our
+                    fitness club</h2>
+            </header>
+            <p>Profit is the fitness club that makes fitness totally accessible to everybody. We promise to help people from all walks of life achieve their individual health and fitness goals — no matter their shape, age, and experience.</p>
+        </section>
+    )
+}
+
+export default Section4
