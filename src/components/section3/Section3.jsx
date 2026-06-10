9791f660e5ab6d8ee5cd44baae249d91db665607
arath abello <arathabello60@gmail.com>
2026-06-09 21:16:41 -0400

    descargando imagenes y section 3 terminado

---
+++ src/components/section3/Section3.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './section3.scss'
+const Section3 = () => {
+    return (
+        <div className='bgImage2'>
+            <div className='workout'>
+                <p>only 5 days</p>
+                <h2 className='workoutH2'>start your <br />
+                    workout now</h2>
+                <p>Enter your name and email in the form below to get your first free workout at Profit.</p>
+
+
+                <form className='form'>
+                    <div>
+                        <input type="text" placeholder='your name' required />
+                    </div>
+                    <div>
+                        <input type="email" placeholder='E-mail' required />
+                    </div>
+
+                    <button type='submit' className='linkJoin'>JOIN NOW</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Section3
